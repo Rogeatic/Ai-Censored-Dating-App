d063698f0164a82deccb1d7c87742a33f311e1da
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-04-28 19:35:43 -0600

    Initial Commit

---
+++ BlurrrDatingApp/BlurrrDatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurrrDatingAppApp.swift
+//  BlurrrDatingApp
+//
+//  Created by Logan Bell on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BlurrrDatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
